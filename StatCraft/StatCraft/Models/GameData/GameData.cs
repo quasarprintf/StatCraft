7ae8ed52eb7fab7249e41892a109216f59ee8927
printf <you@example.com>
2026-07-22 08:55:11 -0400

    Human - create initial GameData model. Not complete

---
+++ StatCraft/StatCraft/Models/GameData/GameData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatCraft.Models.GameData
+{
+    internal class GameData
+    {
+        public int? GameId { get; set; }
+        public required ReplayData ReplayData { get; set; }
+        public int? BuildId { get; set; }
+        public string Notes { get; set; } = string.Empty;
+    }
+}
