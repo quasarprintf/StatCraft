17c3a28c1afdc50fb91b3e841e44b20e63ab2369
printf <you@example.com>
2026-07-29 12:36:41 -0400

    Human - rename colors

--- StatCraft/StatCraft/Styles/Colors.cs
+++ StatCraft/StatCraft/Styles/Colors.cs
@@ -4,8 +4,8 @@ namespace StatCraft.Styles
 {
     public static class Colors
     {
-        public static readonly IBrush VibrantGreen = new SolidColorBrush(Color.Parse("#00CC1B"));
-        public static readonly IBrush VibrantBlue = Brushes.Blue;
-        public static readonly IBrush VibrantRed = Brushes.Red;
+        public static readonly IBrush ProtossGreen = new SolidColorBrush(Color.Parse("#00CC1B"));
+        public static readonly IBrush TerranBlue = Brushes.Blue;
+        public static readonly IBrush ZergRed = Brushes.Red;
     }
 }
--- StatCraft/StatCraft/ViewModels/Windows/DataComponents/GameDataRowViewModel.cs
+++ StatCraft/StatCraft/ViewModels/Windows/DataComponents/GameDataRowViewModel.cs
@@ -49,7 +49,7 @@ namespace StatCraft.ViewModels
             ParsedReplayData replay = game.ReplayData;
             MapName = replay.MapName;
             ResultLabel = replay.Win == 1m ? "Win" : replay.Win == 0m ? "Loss" : "Draw";
-            ResultColor = replay.Win == 1m ? Styles.Colors.VibrantGreen : replay.Win == 0m ? Styles.Colors.VibrantRed : Styles.Colors.VibrantBlue;
+            ResultColor = replay.Win == 1m ? Styles.Colors.ProtossGreen : replay.Win == 0m ? Styles.Colors.ZergRed : Styles.Colors.TerranBlue;
             GameLength = TimeSpan.FromSeconds(replay.GameLengthSeconds).ToString(@"mm\:ss");
             Matchup = $"{replay.Player.Race}{string.Concat(replay.Allies.Select(a => a.Race))}v{string.Concat(replay.Opponents.Select(o => o.Race))}";
             MatchupCharacters = BuildMatchupCharacters(replay);
@@ -94,9 +94,9 @@ namespace StatCraft.ViewModels
 
         private static IBrush RaceColor(char race) => race switch
         {
-            'P' => Styles.Colors.VibrantGreen,
-            'T' => Styles.Colors.VibrantBlue,
-            'Z' => Styles.Colors.VibrantRed,
+            'P' => Styles.Colors.ProtossGreen,
+            'T' => Styles.Colors.TerranBlue,
+            'Z' => Styles.Colors.ZergRed,
             _ => Brushes.Gray,
         };
 
