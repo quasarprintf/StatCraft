cc2b3b9ebf9759a3204d1b9fbc533e564777cfcd
printf <you@example.com>
2026-07-31 08:42:28 -0400

    Human - remove pointless comment

--- StatCraft/StatCraft/Models/GameData/GamePlayer.cs
+++ StatCraft/StatCraft/Models/GameData/GamePlayer.cs
@@ -6,9 +6,6 @@ namespace StatCraft.Models.GameData
 {
     internal class GamePlayer
     {
-        // Id of this player's own GamePlayers row, once persisted. GameBuilds/GameAttributeValues
-        // reference this rather than the game itself, since build/attribute tracking is inherently about
-        // one specific player's performance in a game.
         public int? GamePlayerId { get; set; }
 
         public required string Clan { get; set; }
