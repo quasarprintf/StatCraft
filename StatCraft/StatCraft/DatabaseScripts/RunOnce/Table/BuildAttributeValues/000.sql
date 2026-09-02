719103d5ca0d776e67c9d3b333921fa6aaca59b3
printf <you@example.com>
2026-09-02 12:04:19 -0400

    Human - create new BuildAttributeValues table for static build attributes

---
+++ StatCraft/StatCraft/DatabaseScripts/RunOnce/Table/BuildAttributeValues/000.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE BuildAttributeValues (
+    Id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    BuildId       INTEGER NOT NULL REFERENCES BuildNodes(Id) ON DELETE CASCADE,
+    AttributeId INTEGER NOT NULL REFERENCES AttributeDefinitions(Id) ON DELETE CASCADE,
+    Value       TEXT    NOT NULL DEFAULT '',
+    UNIQUE(BuildId, AttributeId)
+);
